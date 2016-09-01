928c954a17f6b9771215d13674b7aeb86b78c7da
birkin <birkin.diana@gmail.com>
2016-09-01 10:19:46 -0400

    adds failing test for issue.

--- ebook_finder/tests.py
+++ ebook_finder/tests.py
@@ -18,7 +18,7 @@ class ProcessorTest( TestCase ):
         self.slr_data = SolrData()
         self.processor = Processor()
 
-    def test_massage_data( self ):
+    def test_massage_data__author_title_A( self ):
         """ Checks parse of solr response. """
         dct_resp = self.slr_data.author_title_response_A
         self.assertEqual(
@@ -31,6 +31,24 @@ class ProcessorTest( TestCase ):
             self.processor.massage_data( verbosity='full', raw_data_dct=dct_resp )
             )
 
+    def test_massage_data__author_title_B( self ):
+        """ Checks parse of solr response. """
+        dct_resp = self.slr_data.author_title_response_B
+        self.assertEqual(
+            [{u'author': u'Bald, Vivek',
+              u'bib': u'b7119857',
+              u'language': [u'English'],
+              u'pub_date': [u'2013'],
+              u'title': u'Bengali Harlem and the lost histories of South Asian America',
+              u'url': u'https://login.revproxy.brown.edu/login?url=http://site.ebrary.com/lib/brown/Doc?id=10642232'},
+             {u'author': u'Bald, Vivek',
+              u'bib': u'b7154298',
+              u'language': [u'English'],
+              u'pub_date': [u'2012'],
+              u'title': u'Bengali Harlem and the lost histories of South Asian America',
+              u'url': u'https://login.revproxy.brown.edu/login?url=http://search.ebscohost.com/login.aspx?direct=true&scope=site&db=e000xna&AN=502790'}],
+            self.processor.massage_data( verbosity='full', raw_data_dct=dct_resp )
+            )
 
 # class SolrAccessorTest( TestCase ):
 #     """ Tests models.SolrAccessor() """
@@ -87,6 +105,71 @@ class SolrData( object ):
                u'fq': [u'{!raw f=format}Book', u'access_facet:("Online")'],
                u'indent': u'true',
                u'q': u'_query_:"{!dismax spellcheck.dictionary=title qf=$title_qf pf=$title_pf}Cruising utopia" AND _query_:"{!dismax spellcheck.dictionary=author qf=$author_qf pf=$author_pf}Munoz"',
+               u'rows': u'10',
+               u'sort': u'score desc, pub_date_sort desc, title_sort asc',
+               u'wt': u'json'},
+              u'status': 0}}
+        self.author_title_response_B = {
+             u'facet_counts': {u'facet_dates': {},
+              u'facet_fields': {u'format': [u'Book', 3],
+               u'language_facet': [u'English', 3],
+               u'lc_1letter_facet': [],
+               u'lc_alpha_facet': [],
+               u'lc_b4cutter_facet': [],
+               u'pub_date': [u'2013', 2, u'2012', 1],
+               u'subject_era_facet': [],
+               u'subject_geo_facet': [],
+               u'subject_topic_facet': []},
+              u'facet_queries': {},
+              u'facet_ranges': {}},
+             u'response': {u'docs': [{u'author_display': u'Bald, Vivek',
+                u'format': u'Book',
+                u'id': u'b6284150',
+                u'isbn_t': [u'9780674066663',
+                 u'0674066669',
+                 u'9780674067578 (ebook)',
+                 u'0674067576 (ebook)'],
+                u'language_facet': [u'English'],
+                u'pub_date': [u'2013'],
+                u'score': 25.486412,
+                u'title_display': u'Bengali Harlem and the lost histories of South Asian America'},
+               {u'author_display': u'Bald, Vivek',
+                u'format': u'Book',
+                u'id': u'b7119857',
+                u'isbn_t': [u'9780674066663',
+                 u'0674066669',
+                 u'9780674067578 (ebook)',
+                 u'0674067576 (ebook)'],
+                u'language_facet': [u'English'],
+                u'pub_date': [u'2013'],
+                u'published_display': [u'Cambridge, Mass'],
+                u'score': 25.486412,
+                u'title_display': u'Bengali Harlem and the lost histories of South Asian America',
+                u'url_fulltext_display': [u'https://login.revproxy.brown.edu/login?url=http://site.ebrary.com/lib/brown/Doc?id=10642232'],
+                u'url_suppl_display': [u'Full text available from Ebrary Academic Complete Subscription Collection']},
+               {u'author_display': u'Bald, Vivek',
+                u'format': u'Book',
+                u'id': u'b7154298',
+                u'isbn_t': [u'9780674067578 (electronic bk.)',
+                 u'0674067576 (electronic bk.)',
+                 u'9780674066663',
+                 u'0674066669'],
+                u'language_facet': [u'English'],
+                u'pub_date': [u'2012'],
+                u'published_display': [u'Cambridge, Mass'],
+                u'score': 25.486412,
+                u'title_display': u'Bengali Harlem and the lost histories of South Asian America',
+                u'url_fulltext_display': [u'https://login.revproxy.brown.edu/login?url=http://search.ebscohost.com/login.aspx?direct=true&scope=site&db=e000xna&AN=502790'],
+                u'url_suppl_display': [u'Full text available from EBSCO eBook Academic Subscription Collection']}],
+              u'maxScore': 25.486412,
+              u'numFound': 3,
+              u'start': 0},
+             u'responseHeader': {u'QTime': 24,
+              u'params': {u'defType': u'lucene',
+               u'f.author_facet.facet.limit': u'21',
+               u'fq': [u'{!raw f=format}Book', u'access_facet:("Online")'],
+               u'indent': u'true',
+               u'q': u'_query_:"{!dismax spellcheck.dictionary=title qf=$title_qf pf=$title_pf}Bengali Harlem and the lost histories of South Asian America" AND _query_:"{!dismax spellcheck.dictionary=author qf=$author_qf pf=$author_pf}Bald, Vivek"',
                u'rows': u'10',
                u'sort': u'score desc, pub_date_sort desc, title_sort asc',
                u'wt': u'json'},
